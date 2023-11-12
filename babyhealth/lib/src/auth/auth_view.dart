82d1fb8b2382b1803cdc9ce32b0d0f95fddfbf80
Yana <yani.krivitskaya@gmail.com>
2023-11-12 22:34:18 +0300

    [upd] - minor upd

--- babyhealth/lib/src/auth/auth_view.dart
+++ babyhealth/lib/src/auth/auth_view.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class AuthView extends StatefulWidget{
+  final GlobalKey<ScaffoldState> _scaffoldKey;
+
+  AuthView(this._scaffoldKey);
+
+  State<AuthView> createState() => _AuthViewState();
+}
+
+class _AuthViewState extends State<AuthView>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
